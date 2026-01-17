8d1d742e24ae5af05a233b90e2918120dfc6e84d
Jampa Pranav Teja <pranavtejajampa@gmail.com>
2026-01-17 19:09:31 +0530

    Add Ichihime and Triangle [A] from Codeforces

---
+++ 1337/A - Ichihime and Triangle/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {    int t;    cin >> t;    while(t-- > 0){      int a, b, c, d;      cin >> a >> b >> c >> d;      int x, y, z;      z = c;      y = c;      x = a;      cout << x << " " << y << " " << z << endl;     }  }
