991d1bb1d60eb126df2729bc8a772b0297d9dd25
Jampa Pranav Teja <pranavtejajampa@gmail.com>
2026-01-18 20:24:02 +0530

    Add Perfect Root [A] from Codeforces

---
+++ 2185/A - Perfect Root/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {    int t;    cin >> t;    while(t-- > 0){      int n;      cin >> n;      int i=1;      while(n>0){        cout << i << " ";        i++;        n--;      }      cout << endl;          }      }
